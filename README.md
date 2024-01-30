03a3fe118bbdeb0c3c3018238d268f286e758229
dnicholson314 <143367057+dnicholson314@users.noreply.github.com>
2024-01-30 15:47:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# PDF Annotation Scraper
+
+This application will redact the authors from any annotations in a PDF.
+It is for use in applications where knowing the authors of annotations is
+undesirable: for example, a double-blind journal where revisions are sent
+directly to authors as PDFs.
+
+I created this application for use at the Rawlings School of Divinity
+journal [_Eleutheria_](https://digitalcommons.liberty.edu/eleu/). As a result,
+it is tailored for personal use. However, pull requests are welcomed if any
+issues arise with the application.
+
+## Requirements
+
+The application was built in Python 3.12 but should work for any Python
+version since 3.8.
+
+In terms of dependencies, `requirements.txt` contains all the necessary packages,
+and the following instructions work through installing those packages for
+use on your computer.
+
+## Instructions
+
+First, clone the repository to a local folder:
+
+```bash
+git clone https://github.com/dnicholson314/PDF-Annotation-Scraper.git
+```
+
+Then open a terminal and navigate to the working directory. Run the following
+command to install the dependencies:
+
+```bash
+pip install requirements.txt
+```
+
+Finally, run the following command to launch the application:
+
+```bash
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,93 @@
+import tkinter as tk
+from tkinter import filedialog
+
+import traceback as tb
+
+from pypdf import PdfReader, PdfWriter
+from pypdf.generic import NameObject, TextStringObject, IndirectObject
+from pypdf.errors import PdfStreamError
+
+
+greeting_message = """\
+    Hello! This application will redact the author names from all annotations
+    in a given pdf. Once you press ENTER, you will be presented with a file
+    dialog to choose the PDF you would like to redact the author names from.
+    Once the names are redacted, the application will return a success message.
+
+    Press ENTER to continue.
+"""
+
+success_message = """
+    Successfully redacted author names from annotations!
+
+    Press ENTER to quit.
+"""
+
+COLORS = {
+    "WARNING": '\033[93m',
+    "ENDC": '\033[0m',
+}
+
+def str_with_color(string: str, color: str) -> str:
+    end = COLORS["ENDC"]
+    if color not in COLORS.values() or color == end:
+        print(":)")
+        return string
+
+    return color + string + end
+
+def handle_exception(e):
+    print()
+    print("Encountered exception ----------------------------------")
+    tb.print_exception(e)
+    print("--------------------------------------------------------")
+    input("Press ENTER to continue.")
+
+def redact_annotation_author(annot: IndirectObject):
+    obj = annot.get_object()
+    if "/T" not in obj:
+        return
+
+    obj.update(
+        {
+            NameObject("/T"): TextStringObject("")
+        }
+    )
+
+def main():
+    tk.Tk().withdraw()
+
+    input(greeting_message)
+
+    file_dir = filedialog.askopenfilename()
+
+    try:
+        reader = PdfReader(file_dir)
+    except FileNotFoundError as e:
+        raise FileNotFoundError(
+           str_with_color("Did you close the file dialog without opening a file?",
+                          COLORS["WARNING"])
+        ) from e
+    except PdfStreamError as e:
+        raise PdfStreamError(
+            str_with_color("Did you choose a file that was not a .pdf?",
+                           COLORS["WARNING"])
+        ) from e
+
+    writer = PdfWriter()
+
+    for page in reader.pages:
+        if "/Annots" in page:
+            for annot in page["/Annots"]:
+                redact_annotation_author(annot)
+
+        writer.add_page(page)
+
+    with open(file_dir, "wb") as fp:
+        writer.write(fp)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        handle_exception(e)
